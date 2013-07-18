04bd8955de5538e498fd18b6bd3ba3b30601894c
sweenee <sweenee1@tcnj.edu>
2013-07-18 09:21:41 -0400

    iterations of Z with left hand sum

--- simulations/MATLAB/Zrepeat.m
+++ simulations/MATLAB/Zrepeat.m
@@ -1,9 +1,9 @@
 fp=fopen('Z.csv','w');
 fprintf(fp,'Z\n');
 
-for i=1:1000
-    [Z, X] = DeerPop(10, 300, 30, 20, 3000000, 1);
-     fprintf(fp,'%f\n',Z(300+1)); %records last position Z
+for i=1:10000
+    [Z, X] = DeerPop(10, 1000, 30, 20, 3000000, 1);
+     fprintf(fp,'%f\n',Z(1000+1)); %records last position Z
 end
 
 fclose(fp); 
