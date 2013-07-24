9b26cde886000dd0a1685d744cb6052804d1539e
candaceliu <liu.candace@gmail.com>
2013-07-24 12:34:48 -0400

    mean and variance vectors

--- simulations/MATLAB/MeanVar_DeerInsSim.m
+++ simulations/MATLAB/MeanVar_DeerInsSim.m
@@ -2,23 +2,29 @@ function [ Xmean, Xvar, Mmean, Mvar ] = MeanVar_DeerInsSim( alpha, gamma, P )
 %UNTITLED4 Summary of this function goes here
 %   Detailed explanation goes here
 
-%Problem Parameters
+iterations = 10;
+
 T = 10;
 N = 10000; %start with 10000 then use 25000
 r1 = 1.7;
 h = .16;
 F = 28000;
 rho = .004; %log(1+rate)
-beta = .003*3000; %
+beta = 9; %.003*3000
 g = .04;
 
-for i=1:10
+Xvec = zeros(1, iterations);
+Mvec = zeros(1, iterations);
+
+for i=1:iterations
     [Xtrue, Mem] = DeerInsSim( T, N, r1, h, F, alpha, rho, beta, P, gamma, g );
+    Xvec(1,i) = Xtrue(N+1);
+    Mvec(1,i) = Mem(N+1);
 end
 
-Xmean = mean(Xtrue);
-Xvar = var(Xtrue);
-Mmean = mean(Mem);
-Mvar = var(Mem);
+Xmean = mean(Xvec);
+Xvar = var(Xvec);
+Mmean = mean(Mvec);
+Mvar = var(Mvec);
 
 
