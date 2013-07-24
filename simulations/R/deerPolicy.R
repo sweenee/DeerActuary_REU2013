cb67a3d31da66b4a6cd90f36c09ccc64ea89bf49
lindongzhou <zhoul@potsdam.edu>
2013-07-24 11:31:16 -0400

    R simulation update

--- simulations/R/deerPolicy.R
+++ simulations/R/deerPolicy.R
@@ -9,26 +9,26 @@
 TESTING         = TRUE   # Will print graphs if true
 INITIALIZE_SEED = FALSE  # Will initialize the seed if true
 
-if(TESTING) {
+if (TESTING) {
   # The script is set up for testing purposes.
   
-  if(INITIALIZE_SEED) {
+  if (INITIALIZE_SEED) {
     # Set the seed using the value in the data file.
     load('randomSeed.Rdata')
     set.seed(theSeed)
   }
   
   # Reset the graphics context
-  if(length(dev.list())>0) {
+  if (length(dev.list()) > 0) {
     dev.off()
   }
-  par(mfrow=c(2,2))
+  par(mfrow = c(2, 2))
 }
 
 T <- 10       # Set the final time
 N <- 10000    # Set the number of sample points
-r <- 1.54			# r tilde - The scaled growth factor
-F <- 24781	  # F tilde - The scaled carrying capacity
+r <- 1.5			# r tilde - The scaled growth factor
+F <- 25000	  # F tilde - The scaled carrying capacity
 alpha <- 0.1  # Noise coeff. in the deer equation.
 
 # Calculate the time step and generate the random numbers
@@ -48,9 +48,9 @@ phi <- exp((a - 0.5 * b ^ 2) * seq(0, T, dt) - b * w)
 # Approximate the stochastic integral for the z equation.
 # TODO - convert to a Milstein approximation.
 integral <- vector(length = N + 1)
-integral <- c(0, cumsum(exp((0.5 * b ^ 2 - a) * t[1:N] + b * w[1:N]) * dw + 0.5 
-                        * b * exp((0.5 * b ^ 2 - a) * t[1:N] + b * w[1:N])
-                        * (dw * dw - dt)))
+integral <- c(0, cumsum(exp((0.5 * b ^ 2 - a) * t[1:N] + b * w[1:N]) * dw + 
+                          0.5 * b * exp((0.5 * b ^ 2 - a) * t[1:N] + 
+                                          b * w[1:N]) * (dw * dw - dt)))
 
 # Calculate the values of Z based on the approximation of the integral
 # above. Then undo the transformation to calculate the population.
@@ -58,7 +58,7 @@ Ztrue <- r/(0.5 * b ^ 2 - a) + g0 * phi -
   b * r/(0.5 * b ^ 2 - a) * phi * integral
 Xtrue <- F/Ztrue
 
-if(TESTING) {
+if (TESTING) {
   # Plot the deer population.
   plot(t, Xtrue,
        type = "l", col = "orange", lwd = 2,       
@@ -80,35 +80,26 @@ for (i in 1:L) {
   Xmil[i + 1] <- Xtemp
 }
 
-if(TESTING) {
+if (TESTING) {
   # Add these new approximations to the plot.
   courseTime = seq(0, T, Dt);
-  points(courseTime, Xmil, col = "green", pch = 4, lwd = 2)
+  points(courseTime, Xmil, col = "green", pch = 4)
 }
-
 
 # Now transform the value of z into the population Distribution
 Z10 <- vector(length = 1000)
 for (i in 1:1000) {
-  dt <- T/N
   dw <- sqrt(dt) * rnorm(N)
-  w <- c(0, cumsum(dw[1:(N - 1)]))
-  x0 <- F
-  z0 <- F/x0
-  a <- alpha ^ 2 - r
-  b <- alpha
-  g0 <- 1 - r/(0.5 * b ^ 2 - a)
-  phi <- exp((a - 0.5 * b ^ 2) * T - b * w[N])
+  w <- c(0, cumsum(dw))
+  phi <- exp((a - 0.5 * b ^ 2) * T - b * w[N + 1])
   integral <- 0
-  ds <- dt
-  s = seq(0, T - ds, ds)
-  integral = sum(exp((0.5 * b ^ 2 - a) * s + b * w) * dw)
+  integral <- sum(exp((0.5 * b ^ 2 - a) * t[1:N] + b * w[1:N]) * dw)
   Ztrue <- r/(0.5 * b ^ 2 - a) +
     g0 * phi - b * r/(0.5 * b ^ 2 - a) * phi * integral
   Z10[i] <- Ztrue
 }
 
-if(TESTING) {
+if (TESTING) {
   cat("The sample mean of Z10 is ",mean(Z10),"\n",
       "The sample standard deviation of Z10 is ",sd(Z10),"\n");
   hist(Z10, probability = TRUE, xlab = "Z", ylab = "Frequency density",
@@ -119,9 +110,9 @@ if(TESTING) {
 
 # Approximate the amount of money in the bond fund
 # First set the parameters used for the bond fund equation
-rho <- 0.04    # Government bond rate
-beta <- 0.1     # Cost of claim proportional to population
-P <- 600       # Premium
+rho <- 0.004    # Government bond rate
+beta <- 9     # Cost of claim proportional to population
+P <- 225000       # Premium
 gamma <- 0.1    # Noise coefficient
 g <- 0.04       # Profit margin
 
@@ -137,7 +128,7 @@ for (i in 1:N) {
   Mmil[i + 1] <- Mtemp
 }
 
-if(TESTING) {
+if (TESTING) {
   # Plot the value of the funds
   plot(t, Mmil, type = "p", pch = 4, col = "purple",
        xlab = "T", ylab = "M", main = "Insurance Payout")
@@ -147,16 +138,16 @@ if(TESTING) {
 
 T <- 10       # Set the final time
 N <- 100    # Set the number of sample points
-r <- 1.54  		# r tilde - The scaled growth factor
-F <- 24781	  # F tilde - The scaled carrying capacity
+r <- 1.5  		# r tilde - The scaled growth factor
+F <- 25000	  # F tilde - The scaled carrying capacity
 dt <- T/N     # Calculate the time step
 
 # Set the initial values
 x0 <- F
 z0 <- F/x0
 
-rho <- 0.04    # Government bond rate
-beta <- 0.24     # Cost of claim proportional to population
+rho <- 0.004    # Government bond rate
+beta <- 9     # Cost of claim proportional to population
 g <- 0.04       # Profit margin
 
 approx <- function(alpha, gamma, P) {   # Noise coefficient and premium
@@ -188,7 +179,6 @@ approx <- function(alpha, gamma, P) {   # Noise coefficient and premium
   Mmil <- vector(length = N + 1)
   Mmil[1] <- m0
   Mtemp <- m0
-  Xtrue <- population(alpha)
   for (i in 1:N) {
     Mtemp <- Mtemp + dt * (rho * Mtemp - beta * Xtrue[i] + P) -
       dw[i] * gamma * Xtrue[i] +
@@ -226,4 +216,4 @@ for (i in seq(0.02, 0.1, 0.02)) {
 deerSim <- data.frame(alpha = alpha, gamma = gamma, P = P, Xmean = Xmean, 
                       Xvar = Xvar, Mmean = Mmean, Mvar = Mvar)
 setwd("C:/Users/Linton/Desktop")
-write.csv(deerSim, file = "deerSim.csv", row.names = FALSE, col.names = TRUE)
+write.csv(deerSim, file = "deerSim.csv", row.names = FALSE)
