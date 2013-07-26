945728020af65dee67c197ab3ee3697060af4d49
sweenee <sweenee1@tcnj.edu>
2013-07-26 14:06:45 -0400

    edits for same W (euler)

--- simulations/MATLAB/MeanVar_csv.m
+++ simulations/MATLAB/MeanVar_csv.m
@@ -1,10 +1,11 @@
 function [  ] = MeanVar_csv( alpha, alpha0, dalpha, gamma, gamma0, dgamma, P, P0, dP )
-%UNTITLED5 Summary of this function goes here
+%Wraps MeanVar_DeerInsSim and records means and variances for different
+%values of alhpa, gamma, and P (our unknown parameters)
 %   Detailed explanation goes here
 
 %{
 do this at least once before the function to create header
-fp = fopen('meanvar.csv','w');
+fp = fopen('meanvarEM.csv','w');
 fprintf(fp,'alpha, gamma, P, Xmean, Xvar, Mmean, Mvar\n');
 fclose(fp);
 %}
@@ -14,7 +15,7 @@ tic
 for i=alpha0:dalpha:alpha
     for j=gamma0:dgamma:gamma
         for k=P0:dP:P
-            fp=fopen('meanvar5000.csv','a');
+            fp=fopen('meanvarEM.csv','a');
             [Xmean, Xvar, Mmean, Mvar] = MeanVar_DeerInsSim(i, j, k);
             fprintf(fp,'%f, %f, %f, %f, %f, %f, %f\n',i, j, k, Xmean, Xvar, Mmean, Mvar);
             fclose(fp);
